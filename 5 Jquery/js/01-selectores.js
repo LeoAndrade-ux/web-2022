98249d983efb9c136fad7992cd7757ac61ac2616
Leo_Andrade <leomijail@hotmail.com>
2022-06-14 10:28:38 -0500

    format

--- 5 Jquery/js/01-selectores.js
+++ 5 Jquery/js/01-selectores.js
@@ -1,43 +1,43 @@
 //evento ready: verifica que toda la pagina este cargada
-$(document).ready(function(){
+$(document).ready(function () {
 
-//seleccion de parrafos por id
-var rojo=$("#rojo").css("background","red");
-//encadenamiento de elementos
-var amarillo=$("#amarillo").css("background","yellow")
-                           .css("color","red");
+    //seleccion de parrafos por id
+    var rojo = $("#rojo").css("background", "red");
+    //encadenamiento de elementos
+    var amarillo = $("#amarillo").css("background", "yellow")
+        .css("color", "red");
 
-var verde=$("#verde").css("background","green")
-                           .css("color","white");
+    var verde = $("#verde").css("background", "green")
+        .css("color", "white");
 
-//selectores de clase
-var miClase=$(".zebra");
-    miClase.css("padding","20px");
+    //selectores de clase
+    var miClase = $(".zebra");
+    miClase.css("padding", "20px");
 
-$(".sin_borde").click(function(){
-    $(this).addClass("zebra");
-});
+    $(".sin_borde").click(function () {
+        $(this).addClass("zebra");
+    });
 
-//selectores por etiqueta
-var parrafos=$('p').css("cursor","pointer");
-parrafos.click(function(){
-    var thiss=$(this);
-    if(thiss.hasClass('zebra')){
-        thiss.addClass('grande');
-    }
-});
-//selectores de varias etiquetas
-$('p,a').addClass('nuevaClase');
+    //selectores por etiqueta
+    var parrafos = $('p').css("cursor", "pointer");
+    parrafos.click(function () {
+        var thiss = $(this);
+        if (thiss.hasClass('zebra')) {
+            thiss.addClass('grande');
+        }
+    });
+    //selectores de varias etiquetas
+    $('p,a').addClass('nuevaClase');
 
-//seleccion de atributos
-$('[title="Google"]').css("background","green")
-                     .css("color","white");
-$('[title="Facebook"]').css("background","blue")
-                     .css("color","white");
+    //seleccion de atributos
+    $('[title="Google"]').css("background", "green")
+        .css("color", "white");
+    $('[title="Facebook"]').css("background", "blue")
+        .css("color", "white");
 
-var busqueda=$('#caja').find(".resaltado");
-console.log(busqueda);
-//parent permite saltar etiquetas
-var busqueda2=$('#elemento2').parent().parent().find(".resaltado");
-console.log(busqueda2);
+    var busqueda = $('#caja').find(".resaltado");
+    console.log(busqueda);
+    //parent permite saltar etiquetas
+    var busqueda2 = $('#elemento2').parent().parent().find(".resaltado");
+    console.log(busqueda2);
 });
